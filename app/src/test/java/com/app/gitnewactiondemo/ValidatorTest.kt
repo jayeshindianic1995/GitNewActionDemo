bf7354f07638acf1941ea8aa01f6aeb2caced9ac
JayeshG <jayesh.g@indianic.com>
2023-04-28 10:50:33 +0530

    added test cases for name

--- app/src/test/java/com/app/gitnewactiondemo/ValidatorTest.kt
+++ app/src/test/java/com/app/gitnewactiondemo/ValidatorTest.kt
@@ -6,7 +6,31 @@ import org.junit.Test
 class ValidatorTest {
     @Test
     fun `empty_name`(){
-        val res = Validator.validateName("Test123")
+        val res = Validator.validateName("")
+        Truth.assertThat(res).isFalse()
+    }
+
+    @Test
+    fun `null_name`(){
+        val res = Validator.validateName(null)
+        Truth.assertThat(res).isFalse()
+    }
+
+    @Test
+    fun `digits_in_name`(){
+        val res = Validator.validateName("name123hello")
+        Truth.assertThat(res).isFalse()
+    }
+
+    @Test
+    fun `special_chars_in_name`(){
+        val res = Validator.validateName("demo@hello")
+        Truth.assertThat(res).isFalse()
+    }
+
+    @Test
+    fun `valid_name`(){
+        val res = Validator.validateName("Jayesh")
         Truth.assertThat(res).isTrue()
     }
 }
